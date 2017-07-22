be2ec24dafc3a9c42cbd4a0e614be11249a96702
Darko Miletic <darko.miletic@blackboard.com>
2017-07-22 09:10:17 -0300

    #9 local_xray: new version 11

--- tests/behat/behat_local_xray.php
+++ tests/behat/behat_local_xray.php
@@ -140,8 +140,8 @@ class behat_local_xray extends behat_base {
         }
 
         // Test express theme only when present.
-        $plugins = \core_plugin_manager::instance()->get_enabled_plugins('theme');
-        if (!empty($plugins) and in_array('express', $plugins)) {
+        $plugins = \core_plugin_manager::instance()->get_installed_plugins('theme');
+        if (in_array('express', $plugins)) {
             // Test express templates.
             // Add express template.
             if (get_config('core', 'theme') != 'express') {
